6d71d090a85660e0cd6d2c4ba30a5f78fa241186
Christian Rodriguez <51222582+chris0100@users.noreply.github.com>
2023-01-04 17:29:24 -0500

    Create repository call and save data

---
+++ src/main/java/com/mobile/food/repository/MobileFoodRepository.java
@@ -0,0 +1,19 @@
+package com.mobile.food.repository;
+
+import com.mobile.food.entity.MobileFoodFacility;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MobileFoodRepository extends JpaRepository<MobileFoodFacility, Long> {
+
+    List<MobileFoodFacility> findAllByLocationDescriptionContainingIgnoreCaseOrAddressContainingIgnoreCase(String locationDescription, String address);
+
+    List<MobileFoodFacility> findAllByAddressContainingIgnoreCase(String address);
+
+    List<MobileFoodFacility> findAllByFacilityTypeContainingIgnoreCase(String address);
+
+    List<MobileFoodFacility> findAllByApplicantContainingIgnoreCaseAndFacilityTypeContainingIgnoreCase(String applicant, String facilityType);
+}
