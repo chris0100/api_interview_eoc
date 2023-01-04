78387e4a919d38b332a8973ae90957a962ac4b75
Christian Rodriguez <51222582+chris0100@users.noreply.github.com>
2023-01-04 17:27:59 -0500

    Add initial configuration and csv file
    Define Bean, setting up the H2 database and a data to be consumed

---
+++ src/main/java/com/mobile/food/ApiInterviewEocApplication.java
@@ -0,0 +1,13 @@
+package com.mobile.food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiInterviewEocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiInterviewEocApplication.class, args);
+    }
+
+}
