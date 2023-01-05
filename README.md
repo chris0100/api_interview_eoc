f276f0e31f5a5ccaa9a2cc6705ce894ad19f00d6
Christian Rodriguez <51222582+chris0100@users.noreply.github.com>
2023-01-05 09:35:50 -0500

    Implement Swagger in the config, controller and readme

--- README.md
+++ README.md
@@ -4,6 +4,16 @@
 ## Database
 It uses a H2 in-memory database sqlite database (for easy local test without losing test data after every restart), can be changed easily in the application.yml for any other database.
 
+## OpenAPI & Swagger
+OpenAPI Specification (formerly Swagger Specification) is an API description format for REST APIs. An OpenAPI file allows you to describe your entire API
+
+Swagger is a set of open-source tools built around the OpenAPI Specification that can help you design, build, document and consume REST APIs
+```
+swagger.json: http://localhost:8080/v2/api-docs
+
+swagger-ui: http://localhost:8080/swagger-ui.html
+```
+
 ## Dependencies
 
 This project uses Maven for builds.
--- src/main/java/com/mobile/food/controller/MobileFoodController.java
+++ src/main/java/com/mobile/food/controller/MobileFoodController.java
@@ -2,6 +2,8 @@ package com.mobile.food.controller;
 
 import com.mobile.food.entity.dto.MobileFoodFacilityDTO;
 import com.mobile.food.service.MobileFoodService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -25,13 +27,16 @@ public class MobileFoodController {
     @Autowired
     private MobileFoodService service;
 
+    @ApiOperation(value = "Get a mobile food by locationId")
+    @ApiResponse(code = 200, message = "OK, the data return correctly")
     @GetMapping(path = "/find-by/{locationId}", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<MobileFoodFacilityDTO> getMobilefoodByLocationId(@PathVariable Long locationId){
         return new ResponseEntity<>(service.getById(locationId), HttpStatus.OK);
     }
 
-
+    @ApiOperation(value = "Get a All mobile food")
+    @ApiResponse(code = 200, message = "OK, the data return correctly")
     @GetMapping(path = "/find-all", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<List<MobileFoodFacilityDTO>> getAllMobilefood(){
@@ -39,19 +44,24 @@ public class MobileFoodController {
     }
 
 
+    @ApiOperation(value = "Get a mobile food by locationDescription or address")
+    @ApiResponse(code = 200, message = "OK, the data return correctly")
     @GetMapping(path = "/find-all/location-description-address/{search}", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<List<MobileFoodFacilityDTO>> getAllMobilefoodByLocationDescriptionOrAddress(@PathVariable String search){
         return new ResponseEntity<>(service.getAllByLocationDescriptionOrAddress(search), HttpStatus.OK);
     }
 
-
+    @ApiOperation(value = "Get a mobile food by address")
+    @ApiResponse(code = 200, message = "OK, the data return correctly")
     @GetMapping(path = "/find-all/address/{address}", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<List<MobileFoodFacilityDTO>> getAllMobilefoodByAddress(@PathVariable String address){
         return new ResponseEntity<>(service.getAllByAddress(address), HttpStatus.OK);
     }
 
+    @ApiOperation(value = "Get a mobile food by locationId")
+    @ApiResponse(code = 200, message = "OK, the data return correctly")
     @GetMapping(path = "/find-all/food-items/{foodItems}", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<List<MobileFoodFacilityDTO>> getAllMobilefoodByFoodItems(@PathVariable String foodItems){
@@ -59,12 +69,16 @@ public class MobileFoodController {
     }
 
 
+    @ApiOperation(value = "Get a mobile food by facilityType")
+    @ApiResponse(code = 200, message = "OK, the data return correctly")
     @GetMapping(path = "/find-all/facility-type/{facilityType}", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<List<MobileFoodFacilityDTO>> getAllMobilefoodByFacilityType(@PathVariable String facilityType){
         return new ResponseEntity<>(service.getAllByFacilityType(facilityType), HttpStatus.OK);
     }
 
+    @ApiOperation(value = "Get a mobile food by applicant and facilityType")
+    @ApiResponse(code = 200, message = "OK, the data return correctly")
     @GetMapping(path = "/find-all/applicant/{applicant}/facility-type/{facilityType}", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<List<MobileFoodFacilityDTO>> getAllMobilefoodByApplicantAndFacilityType(@PathVariable String applicant, @PathVariable String facilityType){
@@ -72,6 +86,8 @@ public class MobileFoodController {
     }
 
 
+    @ApiOperation(value = "Get the close mobile foods by latitude and longitude")
+    @ApiResponse(code = 200, message = "OK, the data return correctly")
     @GetMapping(path = "/find-all", produces = MediaType.APPLICATION_JSON_VALUE, params = {"latitude", "longitude"})
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<List<MobileFoodFacilityDTO>> getAllCloseMobilefoodByLatitudeAndLongitude(@RequestParam Double latitude, @RequestParam Double longitude){
